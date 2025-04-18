4703620d00810b53d671a7be2f44867aa82d994c
Osama Hussein <osamahusseinn2@gmail.com>
2025-04-18 14:42:26 -0400

    Set up sprite animation

--- SFML Practice/Main.cpp
+++ SFML Practice/Main.cpp
@@ -43,6 +43,59 @@ int main()
 	// Set the spriteBackground to cover the screen
 	spriteBackground.setPosition(Vector2f(0, 0));
 
+	// Make a tree sprite
+	Texture textureTree;
+	textureTree.loadFromFile("graphics/tree.png");
+
+	Sprite spriteTree(textureTree);
+	//spriteTree.setTexture(textureTree);
+	spriteTree.setPosition(Vector2f(810, 0));
+
+	// Prepare the bee
+	Texture textureBee;
+	textureBee.loadFromFile("graphics/bee.png");
+
+	Sprite spriteBee(textureBee);
+	//spriteBee.setTexture(textureBee);
+	spriteBee.setPosition(Vector2f(0, 800));
+
+	// Is the bee currently moving?
+	bool beeActive = false;
+
+	// How fast can the bee fly
+	float beeSpeed = 0.0f;
+
+	// make 3 cloud sprites from 1 texture
+	Texture textureCloud;
+
+	// Load 1 new texture
+	textureCloud.loadFromFile("graphics/cloud.png");
+
+	// 3 New sprites with the same texture
+	Sprite spriteCloud1(textureCloud);
+	Sprite spriteCloud2(textureCloud);
+	Sprite spriteCloud3(textureCloud);
+	//spriteCloud1.setTexture(textureCloud);
+	//spriteCloud2.setTexture(textureCloud);
+	//spriteCloud3.setTexture(textureCloud);
+	
+	// Position the clouds on the left of the screen at different heights
+	spriteCloud1.setPosition(Vector2f(0, 0));
+	spriteCloud2.setPosition(Vector2f(0, 250));
+	spriteCloud3.setPosition(Vector2f(0, 500));
+
+	// Are the clouds currently on screen?
+	bool cloud1Active = false;
+	bool cloud2Active = false;
+	bool cloud3Active = false;
+
+	// How fast is each cloud?
+	float cloud1Speed = 0.0f;
+	float cloud2Speed = 0.0f;
+	float cloud3Speed = 0.0f;
+
+	// Variables to control time itself
+	Clock clock;
 
 	while (window.isOpen())
 	{
@@ -66,6 +119,132 @@ int main()
 		****************************************
 		*/
 
+		/* The clock.restart() function, as you might expect, restarts the clock. We want to restart the clock every frame
+		so that we can time how long each and every frame takes. In addition, however, it returns the amount of time that
+		has elapsed since the last time we restarted the clock */
+
+		// Measure time
+		Time dt = clock.restart();
+
+		// Setup the bee
+		if (!beeActive)
+		{
+			// How fast is the bee
+			srand((int)time(0)); // Seed the random number generator
+
+			// Get a random number between 200 and 399 and assign the result to beeSpeed
+			beeSpeed = (rand() % 200) + 200;
+
+			// How high is the bee
+			srand((int)time(0) * 10); // Seed the random number generator again
+
+			// Get a random number between 500 and 999 and assign the result to a new float variable called height
+			float height = (rand() % 500) + 500;
+
+			/* Set the position of the bee to 2000 on the x axis (just off - screen to the right) and to whatever height
+			equals on the y axis */
+			spriteBee.setPosition(Vector2f(2000, height));
+
+			beeActive = true; // Set bee active to true afterwards
+		}
+
+		else // Move the bee
+		{
+			spriteBee.setPosition(Vector2f(spriteBee.getPosition().x - (beeSpeed * dt.asSeconds()), 
+				spriteBee.getPosition().y));
+
+			// Has the bee reached the left-hand edge of the screen?
+			if (spriteBee.getPosition().x < -100)
+			{
+				// Set it up ready to be a whole new bee next frame
+				beeActive = false;
+			}
+		}
+
+		// Manage the clouds
+		// Cloud 1
+		if (!cloud1Active)
+		{
+			// How fast is the cloud
+			srand((int)time(0) * 10);
+			cloud1Speed = (rand() % 200);
+
+			// How high is the cloud
+			srand((int)time(0) * 10);
+			float height = (rand() % 150);
+			spriteCloud1.setPosition(Vector2f(-200, height));
+
+			cloud1Active = true;
+		}
+
+		else
+		{
+			spriteCloud1.setPosition(Vector2f(spriteCloud1.getPosition().x + (cloud1Speed * dt.asSeconds()),
+				spriteCloud1.getPosition().y));
+
+			// Has the cloud reached the right hand edge of the screen?
+			if (spriteCloud1.getPosition().x > 1920)
+			{
+				// Set it up ready to be a whole new cloud next frame
+				cloud1Active = false;
+			}
+		}
+
+		// Cloud 2
+		if (!cloud2Active)
+		{
+			// How fast is the cloud
+			srand((int)time(0) * 20);
+			cloud2Speed = (rand() % 200);
+
+			// How high is the cloud
+			srand((int)time(0) * 20);
+			float height = (rand() % 300) - 150;
+			spriteCloud2.setPosition(Vector2f(-200, height));
+
+			cloud2Active = true;
+		}
+
+		else
+		{
+			spriteCloud2.setPosition(Vector2f(spriteCloud2.getPosition().x +(cloud2Speed * dt.asSeconds()),
+				spriteCloud2.getPosition().y));
+
+			// Has the cloud reached the right hand edge of the screen?
+			if (spriteCloud2.getPosition().x > 1920)
+			{
+				// Set it up ready to be a whole new cloud next frame
+				cloud2Active = false;
+			}
+		}
+
+		if (!cloud3Active)
+		{
+			// How fast is the cloud
+			srand((int)time(0) * 30);
+			cloud3Speed = (rand() % 200);
+
+			// How high is the cloud
+			srand((int)time(0) * 30);
+			float height = (rand() % 450) - 150;
+			spriteCloud3.setPosition(Vector2f(-200, height));
+
+			cloud3Active = true;
+		}
+
+		else
+		{
+			spriteCloud3.setPosition(Vector2f(spriteCloud3.getPosition().x + (cloud3Speed * dt.asSeconds()),
+				spriteCloud3.getPosition().y));
+
+			// Has the cloud reached the right hand edge of the screen?
+			if (spriteCloud3.getPosition().x > 1920)
+			{
+				// Set it up ready to be a whole new cloud next frame
+				cloud3Active = false;
+			}
+		}
+
 		/*
 		****************************************
 		Draw the scene
@@ -80,6 +259,17 @@ int main()
 
 		// Draw our game scene here
 		window.draw(spriteBackground);
+
+		// Draw the clouds
+		window.draw(spriteCloud1);
+		window.draw(spriteCloud2);
+		window.draw(spriteCloud3);
+
+		// Draw the tree
+		window.draw(spriteTree);
+
+		// Draw the insect
+		window.draw(spriteBee);
 
 		// Draw our game scene here
 		// Show everything we just drew
