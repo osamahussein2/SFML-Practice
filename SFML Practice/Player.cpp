10cb576032be11cb9a8b1867528eb84bd8abef5f
Osama Hussein <osamahusseinn2@gmail.com>
2025-04-21 21:42:40 -0400

    Initialized Zombie Shooter game

---
+++ SFML Practice/Player.cpp
@@ -0,0 +1,206 @@
+#include "Player.h"
+
+Player::Player() : m_Sprite(Sprite(m_Texture))
+{
+	m_Speed = START_SPEED;
+	m_Health = START_HEALTH;
+	m_MaxHealth = START_HEALTH;
+
+	// Associate a texture with the sprite
+	// !!Watch this space!!
+	m_Texture.loadFromFile("graphics/player (1).png");
+	m_Sprite = Sprite(m_Texture);
+	//m_Sprite.setTexture(m_Texture);
+
+	// Set the origin of the sprite to the center for smooth rotation
+	m_Sprite.setOrigin(Vector2f(25, 25));
+}
+
+void Player::spawn(IntRect arena, Vector2f resolution, int tileSize)
+{
+	// Place the player in the middle of the arena
+	m_Position.x = arena.size.x / 2;
+	m_Position.y = arena.size.y / 2;
+
+	// Copy the details of the arena to the player's m_Arena
+	m_Arena.position.x = arena.position.x;
+	m_Arena.size.x = arena.size.x;
+	m_Arena.position.y = arena.position.y;
+	m_Arena.size.y = arena.size.y;
+
+	// Remember how big the tiles are in this arena
+	m_TileSize = tileSize;
+
+	// Store the resolution for future use
+	m_Resolution.x = resolution.x;
+	m_Resolution.y = resolution.y;
+}
+
+void Player::resetPlayerStats()
+{
+	m_Speed = START_SPEED;
+	m_Health = START_HEALTH;
+	m_MaxHealth = START_HEALTH;
+}
+
+Time Player::getLastHitTime()
+{
+	return m_LastHit;
+}
+bool Player::hit(Time timeHit)
+{
+	/* This if statement checks to see whether the time that's passed in as a parameter is 200 milliseconds further ahead
+	than the time stored in m_LastHit. If it is, m_LastHit is updated with the time passed in and m_Health has 10 deducted
+	from its current value */
+	if (timeHit.asMilliseconds() - m_LastHit.asMilliseconds() > 200)
+	{
+		m_LastHit = timeHit;
+		m_Health -= 10;
+
+		return true;
+	}
+
+	else
+	{
+		return false;
+	}
+}
+
+FloatRect Player::getPosition()
+{
+	return m_Sprite.getGlobalBounds();
+}
+
+Vector2f Player::getCenter()
+{
+	return m_Position;
+}
+
+Angle Player::getRotation()
+{
+	return m_Sprite.getRotation();
+}
+
+Sprite Player::getSprite()
+{
+	return m_Sprite;
+}
+
+int Player::getHealth()
+{
+	return m_Health;
+}
+
+void Player::moveLeft()
+{
+	m_LeftPressed = true;
+}
+
+void Player::moveRight()
+{
+	m_RightPressed = true;
+}
+
+void Player::moveUp()
+{
+	m_UpPressed = true;
+}
+
+void Player::moveDown()
+{
+	m_DownPressed = true;
+}
+
+void Player::stopLeft()
+{
+	m_LeftPressed = false;
+}
+
+void Player::stopRight()
+{
+	m_RightPressed = false;
+}
+
+void Player::stopUp()
+{
+	m_UpPressed = false;
+}
+
+void Player::stopDown()
+{
+	m_DownPressed = false;
+}
+
+void Player::update(float elapsedTime, Vector2i mousePosition)
+{
+	if (m_UpPressed)
+	{
+		m_Position.y -= m_Speed * elapsedTime;
+	}
+
+	if (m_DownPressed)
+	{
+		m_Position.y += m_Speed * elapsedTime;
+	}
+
+	if (m_RightPressed)
+	{
+		m_Position.x += m_Speed * elapsedTime;
+	}
+
+	if (m_LeftPressed)
+	{
+		m_Position.x -= m_Speed * elapsedTime;
+	}
+
+	m_Sprite.setPosition(m_Position);
+
+	// Keep the player in the arena
+	if (m_Position.x > m_Arena.size.x - m_TileSize)
+	{
+		// This calculation will detect the player straying into the wall
+		m_Position.x = m_Arena.size.x - m_TileSize;
+	}
+
+	if (m_Position.x < m_Arena.position.x + m_TileSize)
+	{
+		m_Position.x = m_Arena.position.x + m_TileSize;
+	}
+
+	if (m_Position.y > m_Arena.size.y - m_TileSize)
+	{
+		m_Position.y = m_Arena.size.y - m_TileSize;
+	}
+
+	if (m_Position.y < m_Arena.position.y + m_TileSize)
+	{
+		m_Position.y = m_Arena.position.y + m_TileSize;
+	}
+
+	// Calculate the angle the player is facing
+	float angle = (atan2(mousePosition.y - m_Resolution.y / 2, mousePosition.x - m_Resolution.x / 2) * 180) / 3.141;
+	m_Sprite.setRotation(degrees(angle));
+}
+
+void Player::upgradeSpeed()
+{
+	// 20% speed upgrade
+	m_Speed += (START_SPEED * 0.2);
+}
+
+void Player::upgradeHealth()
+{
+	// 20% max health upgrade
+	m_MaxHealth += (START_HEALTH * 0.2);
+}
+
+void Player::increaseHealthLevel(int amount)
+{
+	m_Health += amount;
+
+	// But not beyond the maximum
+	if (m_Health > m_MaxHealth)
+	{
+		m_Health = m_MaxHealth;
+	}
+}
--- SFML Practice/Pong.cpp
+++ SFML Practice/Pong.cpp
@@ -1,169 +1,169 @@
-#include "Bat.h"
-#include "Ball.h"
-#include <sstream>
-#include <cstdlib>
-#include <SFML/Graphics.hpp>
-
-int main()
-{
-	// Create a video mode object
-	VideoMode vm(Vector2u(1920, 1080));
-
-	// Create and open a window for the game
-	RenderWindow window(vm, "Pong", State::Fullscreen);
-
-	int score = 0;
-	int lives = 3;
-
-	// Create a bat at the bottom center of the screen
-	Bat bat(1920 / 2, 1080 - 20);
-
-	// Create a ball
-	Ball ball(1920 / 2, 0);
-
-	// A cool retro-style font
-	Font font;
-
-	// We will add a ball in the next chapter
-	// Create a Text object called HUD
-	Text hud(font);
-
-	font.openFromFile("fonts/DS-DIGI.ttf");
-
-	// Set the font to our retro-style
-	//hud.setFont(font);
-
-	// Make it nice and big
-	hud.setCharacterSize(75);
-
-	// Choose a color
-	hud.setFillColor(Color::White);
-	hud.setPosition(Vector2f(20, 20));
-
-	// Here is our clock for timing everything
-	Clock clock;
-
-	while (window.isOpen())
-	{
-		/*
-		Handle the player input
-		****************************
-		****************************
-		****************************
-		*/
-
-		//Event event;
-
-		while (const std::optional event = window.pollEvent())
-		{
-			if (event->is<Event::Closed>())
-			{
-				// Quit the game when the window is closed
-				window.close();
-			}
-		}
-
-		// Handle the player quitting
-		if (Keyboard::isKeyPressed(Keyboard::Key::Escape))
-		{
-			window.close();
-		}
-
-		// Handle the pressing and releasing of the arrow keys
-		if (Keyboard::isKeyPressed(Keyboard::Key::Left))
-		{
-			bat.moveLeft();
-		}
-
-		else
-		{
-			bat.stopLeft();
-		}
-
-		if (Keyboard::isKeyPressed(Keyboard::Key::Right))
-		{
-			bat.moveRight();
-		}
-
-		else
-		{
-			bat.stopRight();
-		}
-
-		/*
-		Update the bat, the ball and the HUD
-		*****************************
-		*****************************
-		*****************************
-		*/
-
-		// Update the delta time
-		Time dt = clock.restart();
-
-		bat.update(dt);
-		ball.update(dt);
-
-		// Update the HUD text
-		std::stringstream ss;
-		ss << "Score:" << score << "	Lives:" << lives;
-		hud.setString(ss.str());
-
-		// Handle ball hitting the bottom
-		if (ball.getPosition().position.y > window.getSize().y)
-		{
-			// reverse the ball direction
-			ball.reboundBottom();
-
-			// Remove a life
-			lives--;
-
-			// Check for zero lives
-			if (lives < 1) 
-			{
-				// reset the score
-				score = 0;
-
-				// reset the lives
-				lives = 3;
-			}
-		}
-
-		// Handle ball hitting top
-		if (ball.getPosition().position.y < 0)
-		{
-			ball.reboundBatOrTop();
-
-			// Add a point to the players score
-			score++;
-		}
-
-		// Handle ball hitting sides
-		if (ball.getPosition().position.x < 0 ||
-			ball.getPosition().position.x + ball.getPosition().size.x > window.getSize().x)
-		{
-			ball.reboundSides();
-		}
-
-		// Has the ball hit the bat?
-		if (ball.getPosition().findIntersection(bat.getPosition()))
-		{
-			// Hit detected so reverse the ball and score a point
-			ball.reboundBatOrTop();
-		}
-
-		/*
-		Draw the bat, the ball and the HUD
-		*****************************
-		*****************************
-		*****************************
-		*/
-
-		window.clear();
-		window.draw(hud);
-		window.draw(bat.getShape());
-		window.draw(ball.getShape());
-		window.display();
-	}
-
-	return 0;
-}
+//#include "Bat.h"
+//#include "Ball.h"
+//#include <sstream>
+//#include <cstdlib>
+//#include <SFML/Graphics.hpp>
+//
+//int main()
+//{
+//	// Create a video mode object
+//	VideoMode vm(Vector2u(1920, 1080));
+//
+//	// Create and open a window for the game
+//	RenderWindow window(vm, "Pong", State::Fullscreen);
+//
+//	int score = 0;
+//	int lives = 3;
+//
+//	// Create a bat at the bottom center of the screen
+//	Bat bat(1920 / 2, 1080 - 20);
+//
+//	// Create a ball
+//	Ball ball(1920 / 2, 0);
+//
+//	// A cool retro-style font
+//	Font font;
+//
+//	// We will add a ball in the next chapter
+//	// Create a Text object called HUD
+//	Text hud(font);
+//
+//	font.openFromFile("fonts/DS-DIGI.ttf");
+//
+//	// Set the font to our retro-style
+//	//hud.setFont(font);
+//
+//	// Make it nice and big
+//	hud.setCharacterSize(75);
+//
+//	// Choose a color
+//	hud.setFillColor(Color::White);
+//	hud.setPosition(Vector2f(20, 20));
+//
+//	// Here is our clock for timing everything
+//	Clock clock;
+//
+//	while (window.isOpen())
+//	{
+//		/*
+//		Handle the player input
+//		****************************
+//		****************************
+//		****************************
+//		*/
+//
+//		//Event event;
+//
+//		while (const std::optional event = window.pollEvent())
+//		{
+//			if (event->is<Event::Closed>())
+//			{
+//				// Quit the game when the window is closed
+//				window.close();
+//			}
+//		}
+//
+//		// Handle the player quitting
+//		if (Keyboard::isKeyPressed(Keyboard::Key::Escape))
+//		{
+//			window.close();
+//		}
+//
+//		// Handle the pressing and releasing of the arrow keys
+//		if (Keyboard::isKeyPressed(Keyboard::Key::Left))
+//		{
+//			bat.moveLeft();
+//		}
+//
+//		else
+//		{
+//			bat.stopLeft();
+//		}
+//
+//		if (Keyboard::isKeyPressed(Keyboard::Key::Right))
+//		{
+//			bat.moveRight();
+//		}
+//
+//		else
+//		{
+//			bat.stopRight();
+//		}
+//
+//		/*
+//		Update the bat, the ball and the HUD
+//		*****************************
+//		*****************************
+//		*****************************
+//		*/
+//
+//		// Update the delta time
+//		Time dt = clock.restart();
+//
+//		bat.update(dt);
+//		ball.update(dt);
+//
+//		// Update the HUD text
+//		std::stringstream ss;
+//		ss << "Score:" << score << "	Lives:" << lives;
+//		hud.setString(ss.str());
+//
+//		// Handle ball hitting the bottom
+//		if (ball.getPosition().position.y > window.getSize().y)
+//		{
+//			// reverse the ball direction
+//			ball.reboundBottom();
+//
+//			// Remove a life
+//			lives--;
+//
+//			// Check for zero lives
+//			if (lives < 1) 
+//			{
+//				// reset the score
+//				score = 0;
+//
+//				// reset the lives
+//				lives = 3;
+//			}
+//		}
+//
+//		// Handle ball hitting top
+//		if (ball.getPosition().position.y < 0)
+//		{
+//			ball.reboundBatOrTop();
+//
+//			// Add a point to the players score
+//			score++;
+//		}
+//
+//		// Handle ball hitting sides
+//		if (ball.getPosition().position.x < 0 ||
+//			ball.getPosition().position.x + ball.getPosition().size.x > window.getSize().x)
+//		{
+//			ball.reboundSides();
+//		}
+//
+//		// Has the ball hit the bat?
+//		if (ball.getPosition().findIntersection(bat.getPosition()))
+//		{
+//			// Hit detected so reverse the ball and score a point
+//			ball.reboundBatOrTop();
+//		}
+//
+//		/*
+//		Draw the bat, the ball and the HUD
+//		*****************************
+//		*****************************
+//		*****************************
+//		*/
+//
+//		window.clear();
+//		window.draw(hud);
+//		window.draw(bat.getShape());
+//		window.draw(ball.getShape());
+//		window.display();
+//	}
+//
+//	return 0;
+//}
